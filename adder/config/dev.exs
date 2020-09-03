178e309d77cb0bd5ae3beb316e5d54e86605a2f2
Robin Malhotra <me@rmalhotra.com>
2020-09-03 01:39:54 +0100

    Add pord config
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- adder/config/dev.exs
+++ adder/config/dev.exs
@@ -16,7 +16,7 @@ config :adder, Adder.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :adder, AdderWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: System.get_env("PORT") || 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
