a1663b856e8c9abf2f7f390a727626226bbf2ddb
Robin Malhotra <me@rmalhotra.com>
2020-09-05 00:46:20 +0100

    raw SQL?
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- adder/lib/adder_web/controllers/adding_controller.ex
+++ adder/lib/adder_web/controllers/adding_controller.ex
@@ -4,16 +4,10 @@ defmodule AdderWeb.AddingController do
   require Adder.Repo
 
   def add(conn, _params) do
-    addition =
-      Adder.Addition
-      |> Ecto.Query.first()
-      |> Ecto.Query.lock("FOR UPDATE")
-      |> Adder.Repo.one()
-
-    changeset = Adder.Addition.changeset(addition, %{sum: addition.sum + 1})
-    Adder.Repo.update(changeset)
-
-    json(conn, %{sum: addition.sum + 1})
+    {:ok, _} = Ecto.Adapters.SQL.query(Adder.Repo, "UPDATE Addition SET sum=sum+1")
+    {:ok, results} = Ecto.Adapters.SQL.query(Adder.Repo, "SELECT * from Addition")
+    [[_, sum]] = results.rows
+    json(conn, %{sum: sum})
   end
 
   def reset() do
