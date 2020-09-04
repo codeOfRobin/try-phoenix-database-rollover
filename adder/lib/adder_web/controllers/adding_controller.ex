5ed6e91a5b48e4378b4e571477f75198cfd8722e
Robin Malhotra <me@rmalhotra.com>
2020-09-04 01:28:38 +0100

    stuff

--- adder/lib/adder_web/controllers/adding_controller.ex
+++ adder/lib/adder_web/controllers/adding_controller.ex
@@ -4,9 +4,26 @@ defmodule AdderWeb.AddingController do
   require Adder.Repo
 
   def add(conn, _params) do
-    addition = Adder.Addition |> Ecto.Query.first() |> Adder.Repo.one()
+    addition =
+      Adder.Addition
+      |> Ecto.Query.first()
+      |> Ecto.Query.lock("FOR UPDATE")
+      |> Adder.Repo.one()
+
     changeset = Adder.Addition.changeset(addition, %{sum: addition.sum + 1})
     Adder.Repo.update(changeset)
+
     json(conn, %{sum: addition.sum + 1})
+  end
+
+  def reset() do
+    addition =
+      Adder.Addition
+      |> Ecto.Query.first()
+      |> Ecto.Query.lock("FOR UPDATE")
+      |> Adder.Repo.one()
+
+    changeset = Adder.Addition.changeset(addition, %{sum: 0})
+    Adder.Repo.update(changeset)
   end
 end
