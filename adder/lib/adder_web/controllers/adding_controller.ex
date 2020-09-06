5adc913bc3932306e5386531f76027b344e2e5d4
Robin Malhotra <me@rmalhotra.com>
2020-09-06 23:56:25 +0100

    use transactions
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- adder/lib/adder_web/controllers/adding_controller.ex
+++ adder/lib/adder_web/controllers/adding_controller.ex
@@ -4,10 +4,28 @@ defmodule AdderWeb.AddingController do
   require Adder.Repo
 
   def add(conn, _params) do
+<<<<<<< HEAD
     {:ok, _} = Ecto.Adapters.SQL.query(Adder.Repo, "UPDATE Addition SET sum=sum+1")
     {:ok, results} = Ecto.Adapters.SQL.query(Adder.Repo, "SELECT * from Addition")
     [[_, sum]] = results.rows
     json(conn, %{sum: sum})
+=======
+    {:ok, dict} =
+      Adder.Repo.transaction(fn ->
+        addition =
+          Adder.Addition
+          |> Ecto.Query.first()
+          |> Ecto.Query.lock("FOR UPDATE")
+          |> Adder.Repo.one()
+
+        changeset = Adder.Addition.changeset(addition, %{sum: addition.sum + 1})
+        Adder.Repo.update(changeset)
+
+        %{sum: addition.sum + 1}
+      end)
+
+    json(conn, dict)
+>>>>>>> use transactions
   end
 
   def reset() do
